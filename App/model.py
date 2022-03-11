cc7513ecfec410fdbcd13dd266743bbb5a2e5d6e
Daniel Lozano <j.lozanom@uniandes.edu.co>
2022-03-10 19:34:36 -0500

    progreso 1

--- App/model.py
+++ App/model.py
@@ -25,6 +25,7 @@
  """
 
 
+from datetime import date
 import config as cf
 from DISClib.ADT import list as lt
 from DISClib.Algorithms.Sorting import shellsort as sa
@@ -37,12 +38,190 @@ los mismos.
 
 # Construccion de modelos
 
+def newCatalog():
+    """
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
+    catalog = {'albums': None,
+               'artists': None,
+               'tracks': None}
+
+    catalog['albums'] = lt.newList(datastructure='ARRAY_LIST')
+    catalog['artists'] = lt.newList(datastructure='SINGLE_LINKED', cmpfunction=compareartists)
+    catalog['tracks'] = lt.newList(datastructure='SINGLE_LINKED', cmpfunction=comparetrackname)
+
+    return catalog
+
 # Funciones para agregar informacion al catalogo
 
-# Funciones para creacion de datos
+def addAlbum(catalog, album):
+    """
+    Adiciona un track a la lista de tracks
+    """
+    a = newAlbum(album['name'],album['id'], album['release_date'])
+    lt.addLast(catalog['albums'], a)
+    return catalog
+
+
+def addArtist(catalog, artist):
+    """
+    Adiciona un track a la lista de tracks
+    """
+    at = newTrack(artist['name'], artist['id'])
+    lt.addLast(catalog['artists'], at)
+    return catalog
+
+
+def addTrack(catalog, track):
+    """
+    Adiciona un track a la lista de tracks
+    """
+    t = newTrack(track['name'], track['id'])
+    lt.addLast(catalog['tracks'], t)
+    return catalog
+    
+def newArtist(name):
+    """
+    Crea una nueva estructura para modelar los libros de
+    un autor y su promedio de ratings
+    """
+    artist = {'name': "", "Albums": None,  "average_rating": 0}
+    artist['name'] = name
+    artist['Albums'] = lt.newList('ARRAY_LIST')
+    return artist
+
+def newTrack(name, id):
+    """
+    Esta estructura almancena los tracks utilizados para marcar libros.
+    """
+    track = {'name': '', 'id': '' }
+    track['name'] = name
+    track['id'] = id
+    return track
+
+def newAlbum(name, id, date):
+    album = {'name': '', 'id': '', 'release_date': ''}
+    album['id']= id
+    album['name']= name
+    album['release_date']= date
+    return album
+    
+def getLastThreeAlbums(catalog):
+    
+    """
+    Retorna los mejores libros
+    """
+    tamanhoAlbum = albumSize(catalog)
+    albums = catalog['albums']
+    lastThreeAlbums = lt.newList()
+    for cont in range(tamanhoAlbum-2, tamanhoAlbum+1):
+        album = lt.getElement(albums, cont)
+        lt.addLast(lastThreeAlbums, album)
+    
+    return lastThreeAlbums
+
+def getLastThreeArtists(catalog):
+    
+    """
+    Retorna los mejores libros
+    """
+    tamanhoArtist = artistsSize(catalog)
+    artists = catalog['artists']
+    lastThreeArtists = lt.newList()
+    for cont in range(tamanhoArtist-2, tamanhoArtist+1):
+        artist = lt.getElement(artists, cont)
+        lt.addLast(lastThreeArtists, artist)
+    
+    return lastThreeArtists 
+
+def getLastThreeTracks(catalog):
+    
+    """
+    Retorna los mejores libros
+    """
+    tamanhoTracks = tracksSize(catalog)
+    tracks = catalog['tracks']
+    lastThreeTracks = lt.newList()
+    for cont in range(tamanhoTracks-2, tamanhoTracks+1):
+        track = lt.getElement(tracks, cont)
+        lt.addLast(lastThreeTracks, track)
+    
+    return lastThreeTracks 
+
+def getAlbumsRange(catalog, fechaI, fechaF):
+    
+    # inicio = int(fechaI)
+    # final = int(fechaF)
+    albums = catalog['albums']['elements']
+    merge_sort_result  = merge_sort(albums)
+    ret = []
+    for i in range(len(merge_sort_result)):
+        if merge_sort_result[i]['release_date'] >= fechaI and merge_sort_result[i]['release_date'] <= fechaF:
+            ret.append(merge_sort_result[i])
+        else:
+            continue
+    return ret
+    
+
+# Funciones utilizadas para comparar elementos dentro de una lista
+def compareartists(artistname1, artist):
+    if artistname1.lower() == artist['name'].lower():
+        return 0
+    elif artistname1.lower() > artist['name'].lower():
+        return 1
+    return -1
+
+
+def comparetrackname(name, track):
+    if (name == track['name']):
+        return 0
+    elif (name > track['name']):
+        return 1
+    return -1
+
+def compareratings(album1, album2):
+    return (str(album1['name']) > str(album2['name']))
+# funciones para comparar elementos dentro de algoritmos de ordenamientos
+def sortAlbums(catalog):
+    sa.sort(catalog['albums'], compareratings)
+# Funciones de ordenamiento
+
+def merge_sort(lista):
+    if len(lista) < 2:
+        return lista
+    else:
+        medio = len(lista)//2
+        derecha = merge_sort(lista[:medio])
+        izquierda = merge_sort(lista[medio:])
+        return merge(derecha, izquierda)
+    
+def merge(lista1, lista2):
+    i,j = 0, 0
+    resultado = []
+    while(i<len(lista1) and j <len(lista2)):
+        if(lista1[i]['release_date']<lista2[j]['release_date']):
+            resultado.append(lista1[i])
+            i+=1
+        else: 
+            resultado.append(lista2[j])
+            j+=1
+    resultado += lista1[i:]
+    resultado += lista2[j:]
+    return resultado
+
 
 # Funciones de consulta
 
-# Funciones utilizadas para comparar elementos dentro de una lista
+def albumSize(catalog):
+    return lt.size(catalog['albums'])
 
-# Funciones de ordenamiento
+
+def artistsSize(catalog):
+    return lt.size(catalog['artists'])
+
+
+def tracksSize(catalog):
+    return lt.size(catalog['tracks'])
--- App/view.py
+++ App/view.py
@@ -9,7 +9,7 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (art your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -33,14 +33,69 @@ Presenta el menu de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
+def newController():
+    """
+    Se crea una instancia del controlador
+    """
+    control = controller.newController()
+    return control
 
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2 - Mostrar información de los últimos 3 elementos de c/u")
+    print("3 Listar albumes en un periodo deseado")
+    print("0- Salir del programa")
 
-catalog = None
+def loadData():
+    """
+    Solicita al controlador que cargue los datos en el modelo
+    """
+    albums, artists, tracks = controller.loadData(control)
+    return albums, artists, tracks
 
+def printInfoAlbum(album):
+    """
+    Imprime los mejores libros solicitados
+    """
+    size = lt.size(album)
+    if size:
+        print(' Esta es la información de los últimos 3 álbumes: ')
+        for album in lt.iterator(album):
+            print('Titulo del álbum: ' + album['name'] + '\nid ' + album['id'] + '\nFecha  publicacion: ' + album['release_date'])
+            
+
+def printInfoArtista(artist):
+    """
+    Imprime los mejores libros solicitados
+    """
+    size = lt.size(artist)
+    if size:
+        print('Esta es la información de los últimos 3 artistas: ')
+        for artist in lt.iterator(artist):
+            print('Nombre: ' + artist['name'])
+
+def printInfoTrack(track):
+    """
+    Imprime los mejores libros solicitados
+    """
+    size = lt.size(track)
+    if size:
+        print('Esta es la información de las últimas 3 canciones: ')
+        for track in lt.iterator(track):
+            print('Nombre de la canción: ' + track['name'])
+
+def printAlbumsRange(rango):
+    size = lt.size(rango)
+    if size:
+        print('Albumes en rango dado: ')
+        for track in lt.iterator(rango):
+            print('Albumes: ')
+
+def printTest():
+    print("Esto es una prueba")  
+
+control = newController()
 """
 Menu principal
 """
@@ -49,10 +104,33 @@ while True:
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
+        ab, art, tr = loadData()
+        print('Álbumes cargados: ' + str(ab))
+        print('Artistas cargados: ' + str(art))
+        print('Tracks cargados: ' + str(tr))
 
     elif int(inputs[0]) == 2:
-        pass
+        
+        album = controller.getLastThreeAlbums(control)
+        printInfoAlbum(album)
+        artista = controller.getLastThreeArtists(control)
+        printInfoArtista(artista)
+        track = controller.getLastThreeTracks(control)
+        printInfoTrack(track)
+    
+    elif int(inputs[0]) == 3:
+        fechaI = input("fecha de inicio: ")
+        fechaF = input("fecha final: ")
+        rangoFechas = controller.getAlbumsRange(control, fechaI, fechaF)
+        printAlbumsRange(rangoFechas)
+        print("gracias wuapo :3")
+        
+        
+        
+
+    elif int(inputs[0]) == 0:
+        sys.exit(0)
 
     else:
-        sys.exit(0)
+        continue
 sys.exit(0)
